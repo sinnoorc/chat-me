6f04d7c3063f0b9b64d9f98c72f7905381e70158
sinnoor c <sinnoorc@gmail.com>
2022-05-05 16:14:09 +0530

    refactor: :rocket: updated routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,10 @@ import 'package:get/get.dart';
 
 import 'package:chat_me/app/modules/home/bindings/home_binding.dart';
 import 'package:chat_me/app/modules/home/views/home_view.dart';
+import 'package:chat_me/app/modules/login/bindings/login_binding.dart';
+import 'package:chat_me/app/modules/login/views/login_view.dart';
+import 'package:chat_me/app/modules/user/bindings/user_binding.dart';
+import 'package:chat_me/app/modules/user/views/user_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +19,16 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.USER,
+      page: () => UserView(),
+      binding: UserBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,8 +5,12 @@ abstract class Routes {
   Routes._();
 
   static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const USER = _Paths.USER;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const USER = '/user';
 }
